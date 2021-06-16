76a719efeb77c6b4951debe27ad3ce9008707a34
nAzdAc <nAzdAc15071996@mail.ru>
2021-06-16 19:38:42 +0300

    fix: fix bug with update word

--- routes/userWords.router.js
+++ routes/userWords.router.js
@@ -16,28 +16,49 @@ router.get('/userWords', authMiddleware, async (req, res) => {
 
 router.post('/updateWord', authMiddleware, async (req, res) => {
 	try {
-		const { wordId, name, value, wordName } = req.body;
+		const { wordId, name, value, wordName, wordBody } = req.body;
 		console.log(req.body);
 		const newValue = value === 'true' ? false : true;
 		const message = getUserWordsMessage(name, newValue, wordName);
 		const user = await User.findById(req.user.userId);
 		const userWords = user.words;
-		if (name === 'deleted' && newValue === false) {
-			const newUserWords = await userWords.filter((item) => `${item._id}` != `${wordId}`);
-			const newUser = await User.findByIdAndUpdate(
-				req.user.userId,
-				{
-					$set: { words: newUserWords }
-				},
-				{ new: true }
-			);
-			res.status(200).json({ userWords: newUser.words, message });
+		console.log(newValue);
+		const wordEntity = userWords.find((item) => `${item._id}` == `${wordId}`);
+		console.log('wordEntity', wordEntity);
+		if (wordEntity) {
+			if (name === 'deleted' && newValue === false) {
+				console.log('popal v if');
+				const newUserWords = await userWords.filter((item) => `${item._id}` != `${wordId}`);
+				const newUser = await User.findByIdAndUpdate(
+					req.user.userId,
+					{
+						$set: { words: newUserWords }
+					},
+					{ new: true }
+				);
+				res.status(200).json({ userWords: newUser.words, message });
+			} else {
+				userWords.forEach((item) => {
+					if (`${item._id}` == `${wordId}`) {
+						item[name] = newValue;
+					}
+				});
+				const newUser = await User.findByIdAndUpdate(
+					req.user.userId,
+					{
+						$set: { words: userWords }
+					},
+					{ new: true }
+				);
+				res.status(200).json({ userWords: newUser.words, message });
+			}
 		} else {
-			userWords.forEach((item) => {
-				if (`${item._id}` == `${wordId}`) {
-					item[name] = newValue;
-				}
-			});
+			const newWord = {
+				...wordBody,
+				[name]: newValue
+			};
+			console.log(newWord);
+			userWords.unshift(newWord);
 			const newUser = await User.findByIdAndUpdate(
 				req.user.userId,
 				{
